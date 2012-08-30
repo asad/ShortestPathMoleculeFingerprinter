26da4011717309f8494921a8b77bfcbfe48aa764
Syed Asad Rahman <s9asad@gmail.com>
2012-08-30 12:29:17 +0100

    formatting Signed-off-by:Syed Asad Rahman <s9asad@gmail.com>

--- src/fingerprints/ShortestPathFingerprinter.java
+++ src/fingerprints/ShortestPathFingerprinter.java
@@ -64,7 +64,11 @@ import org.openscience.cdk.tools.periodictable.PeriodicTable;
  * </P>
  *
  *
- * @author Syed Asad Rahman (2012) @cdk.keyword fingerprint @cdk.keyword similarity @cdk.module standard @cdk.githash
+ * @author Syed Asad Rahman (2012) 
+ * @cdk.keyword fingerprint 
+ * @cdk.keyword similarity 
+ * @cdk.module standard 
+ * @cdk.githash
  *
  */
 @TestClass("org.openscience.cdk.fingerprint.ShortestPathFingerprinter")
